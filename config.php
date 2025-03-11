4788770f2755ba7682c1b85208c85e0f1923fdb5
alfa-alfi-nurjanah <alfa.alfi124@smk.belajar.id>
2025-03-11 23:20:02 +0700

    menambahkan config

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// kofigurasi koneksi database
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "hospital_management";
+
+// membuat koneksi ke database menggunakan MySQLi
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// mengecek apakah koneksi berhasil
+if ($conn->connect_error) {
+    // menampilkan pesan error jika koneksi gagal
+    die("Database gagal terkoneksi: " . $conn->connect_error);
+}
+
+// jika oneksi berhasil, script akan terus berjalan tanpa pesan error
+?>
